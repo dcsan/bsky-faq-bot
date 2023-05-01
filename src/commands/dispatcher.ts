a377c3848d3e7c7beaae75b9dfd86bb3b49abb67
dcsan <dc@rik.ai>
2023-05-01 18:35:23 +0200

    add some basic test

--- src/commands/dispatcher.ts
+++ src/commands/dispatcher.ts
@@ -1,6 +1,7 @@
 // call the right command based on first /arg
 import { BskyBot, Events } from "easy-bsky-bot-sdk";
 import { MockBot } from "../test/MockBot";
+import { FaqManager } from "../store/FaqManager";
 
 const clog = console
 
@@ -8,19 +9,28 @@ async function dispatchEvent(event: any, bot: BskyBot | MockBot) {
   const { post } = event;
   const text: string = post.text;
   // TODO word boundary
-  const groups = text.match(/(faq )(.*)$/)
-  if (!groups) {
-    clog.warn('no faq match')
+  const chunks = text.match(/(faq )(.*)$/i)
+  if (!chunks) {
+    clog.warn('no regex match')
     return
   }
-  clog.log('groups', groups)
-  // match has a space in for the command
-  if (groups[1] === 'faq ') {
-    const topic = groups[2]
-    clog.log(`faq found: [${topic}]`)
-    await bot.reply(`faq reply to topic: [${topic}]`, post);
-  } else {
-    clog.log('no faq match', groups)
+  clog.log('faq groups:', chunks)
+
+  if (chunks[1] === 'faq ') {
+    // match is 'faq<space>' for the command - TODO word boundaries
+    const topic = chunks[2]
+    clog.log(`faq topic: [${topic}]`)
+    let msg = ''
+    const faq = await FaqManager.findFaq(topic)
+    if (!faq) {
+      msg = `sorry no faq found for [${topic}]`
+      clog.warn('no answer for topic', topic)
+    } else {
+      // format reply
+      msg = `faq topic: [${faq.topic}]\nℹ️ ${faq.answer}`
+      // TODO add link
+      await bot.reply(msg, post);
+    }
   }
 }
 
--- src/test/dispatcher.test.ts
+++ src/test/dispatcher.test.ts
@@ -3,13 +3,19 @@
 import { MockBot, MockEvent } from "./MockBot";
 import { dispatchEvent } from "../commands/dispatcher";
 
+const clog = console
 
 async function testDispatcher() {
   const bot = new MockBot();
   const event = new MockEvent({
-    text: 'faq moot'
+    text: 'hey bot /faq did'
   })
   await dispatchEvent(event, bot);
+  // TODO export assert
+  if (!bot.lastReply.startsWith('faq topic: [DID]\nℹ️ DIDs are unique global identifiers ')) {
+    throw new Error('testDispatcher failed')
+  }
+  clog.log('testDispatcher passed')
 }
 
 async function main() {
