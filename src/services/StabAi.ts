2396fabb0a0d0afdb87f58d9a7cd2d0832117951
dcsan <dc@rik.ai>
2023-05-16 06:28:37 -0700

    render twks

--- src/services/StabAi.ts
+++ src/services/StabAi.ts
@@ -1,4 +1,3 @@
-// import fetch from 'node-fetch'
 import fs from 'node:fs'
 import { AppConfig } from '../utils/AppConfig'
 import { ensureDir } from '../utils/localFiles'
@@ -6,6 +5,14 @@ import { ensureDir } from '../utils/localFiles'
 const localConfig = {
   // engineId: 'stable-diffusion-v1-5',
   engineId: 'stable-diffusion-xl-beta-v2-2-2',
+}
+
+interface GenerationResponse {
+  artifacts: Array<{
+    base64: string
+    seed: number
+    finishReason: string
+  }>
 }
 
 const clog = console
@@ -17,46 +24,47 @@ export async function genImage(prompt: string, outDir?: string): Promise<string[
   const apiKey = AppConfig.STABILITY_API_KEY
 
   if (!apiKey) throw new Error('Missing Stability API key.')
+  let response: any
 
-  // @ts-ignore
-  const response = await fetch(
-    `${apiHost}/v1/generation/${localConfig.engineId}/text-to-image`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-        Authorization: `Bearer ${apiKey}`,
-      },
-      body: JSON.stringify({
-        text_prompts: [
-          {
-            text: prompt,
-          },
-        ],
-        cfg_scale: 7,
-        clip_guidance_preset: 'FAST_BLUE',
-        height: 512,
-        width: 512,
-        samples: 1,
-        steps: 30,
-      }),
-    }
-  )
+  try {
+    // @ts-ignore
+    response = await fetch(
+      `${apiHost}/v1/generation/${localConfig.engineId}/text-to-image`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          Authorization: `Bearer ${apiKey}`,
+        },
+        body: JSON.stringify({
+          text_prompts: [
+            {
+              text: prompt,
+            },
+          ],
+          cfg_scale: 7,
+          clip_guidance_preset: 'FAST_BLUE',
+          height: 512,
+          width: 512,
+          samples: 1,
+          steps: 30,
+        }),
+      }
+    )
+
+
+  } catch (err) {
+    // timeout?
+    console.error('genImage fetch error', err)
+    return ['']
+  }
 
   if (!response.ok) {
     // TODO retry
     clog.error(`Non-200 response: ${await response.text()}`)
     clog.log('input', { prompt })
     return [""]
-  }
-
-  interface GenerationResponse {
-    artifacts: Array<{
-      base64: string
-      seed: number
-      finishReason: string
-    }>
   }
 
   const responseJSON = (await response.json()) as GenerationResponse
@@ -78,7 +86,8 @@ export async function genImage(prompt: string, outDir?: string): Promise<string[
   return results
 }
 
-function testGen() {
+
+function testGenerate() {
   const results = genImage('A duck walks into a bar.').then((res) => {
     console.log(res)
   }).catch((err) => {
@@ -86,5 +95,6 @@ function testGen() {
   })
   console.log('results', results)
 }
+
 
 // testGen()
